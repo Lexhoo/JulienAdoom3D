44440a8c7e1ab49959d6de52f44d1a061ed19c20
Lexhoo <lexhoo@yahoo.fr>
2020-10-27 23:54:05 +0100

    add url caprover

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -10,7 +10,7 @@ export class EmailService {
 
   constructor(private http :HttpClient) { }
 
-  private baseUrl = 'http://localhost:8080/mail/post';
+  private baseUrl = 'https://doumovieback.doumovie.fr/mail/post';
 
   envoiEmail(email :Email): Observable<any> {
     return this.http.post(`${this.baseUrl}`, email);
