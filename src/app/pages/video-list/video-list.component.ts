18d2a8f8f9d18f725fffb00870c2bbc245b5615f
Lexhoo <lexhoo@yahoo.fr>
2022-08-15 14:36:03 +0200

    up

--- src/app/pages/video-list/video-list.component.ts
+++ src/app/pages/video-list/video-list.component.ts
@@ -11,6 +11,21 @@ import { ProjetService } from '../../services/projet.service';
 })
 export class VideoListComponent  {
   images: UploadFiles[] = [];
+<<<<<<< HEAD
+=======
+  imagesAdvertising: UploadFiles[] = [];
+  imagesOther: UploadFiles[] = [];
+  imagesMakingOf: UploadFiles[] = [];
+  closeResult = '';
+  imagePopin = new UploadFiles();
+  imagesPopin: UploadFiles[] = [];
+  host: string;
+  hasImages: boolean;
+  hasImagesClip: boolean;
+  hasImagesAdvertising: boolean;
+  hasImagesMakingOf: boolean;
+  hasImagesOther: boolean;
+>>>>>>> fbfce27... Modifications suite aux retours de Julien.
 
   constructor(private projetService: ProjetService, private route: ActivatedRoute, private router: Router, private sanitizer: DomSanitizer) {  }
 
@@ -19,7 +34,20 @@ export class VideoListComponent  {
     this.projetService.getImagesVideos().subscribe({
       next: (images) => {
 
+<<<<<<< HEAD
         this.images = images;
+=======
+        this.images = images.filter(image => 1 === image.idSousCategorie);
+        this.hasImages = this.images.length >= 1;
+        this.imagesClip = images.filter(imageClip => 2 === imageClip.idSousCategorie);
+        this.hasImagesClip = this.imagesClip.length >= 1;
+        this.imagesAdvertising = images.filter(imageAdvertising => 3 === imageAdvertising.idSousCategorie);
+        this.hasImagesAdvertising = this.imagesAdvertising.length >= 1;
+        this.imagesMakingOf = images.filter(imageMakingOf => 4 === imageMakingOf.idSousCategorie);
+        this.hasImagesMakingOf = this.imagesMakingOf.length >= 1;
+        this.imagesOther = images.filter(imageOther => 5 === imageOther.idSousCategorie);
+        this.hasImagesOther = this.imagesOther.length >= 1;
+>>>>>>> fbfce27... Modifications suite aux retours de Julien.
       },
       error: (err) => {
         if (err.error.status === 404) {
